e009e0dc0f274e185bcacccdb8f702bfea1297aa
alban genta <alban.genta@edu.ecole-89.com>
2026-03-10 14:38:54 +0100

    added default for partial

--- backend/src/models/entity.py
+++ backend/src/models/entity.py
@@ -4,20 +4,22 @@ from pydantic import BaseModel
 class Entity(BaseModel):
     id: int
     name: str
-    not_discovered_name: str
-    parent: int | None
+    not_discovered_name: str | None = None
+    parent: int | None = None
     universe_id: int
     creator_id: int
 
 class InputEntity(BaseModel):
     name: str
+    not_discoverd_name: str | None = None
     parent: int | None
+    universe_id: int
 
 
 class PartialEntity(BaseModel):
-    id: int | None
-    name: str | None
-    not_discovered_name: str | None
-    parent: int | None
-    universe_id: int | None
-    creator_id: int | None
+    id: int | None = None
+    name: str | None = None
+    not_discovered_name: str | None = None
+    parent: int | None = None
+    universe_id: int | None = None
+    creator_id: int | None = None
