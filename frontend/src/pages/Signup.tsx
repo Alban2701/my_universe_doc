fda4af681caacdd4cdf4d0ee723ade12f1fdffd2
alban genta <alban.genta@edu.ecole-89.com>
2026-03-10 23:02:22 +0100

    replaced fetch ubase url

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -32,7 +32,7 @@ export default function SignUp() {
 			picture: null,
 		};
 		try {
-			const response = await fetch("http://localhost:8000/user/signup", {
+			const response = await fetch("/api/user/signup", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
