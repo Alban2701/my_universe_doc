c7a2d596511f5dd624466af6e7ec637d0b99cf04
alban genta <alban.genta@edu.ecole-89.com>
2026-04-23 17:28:15 +0200

    can now specify what Exception to raise

--- backend/src/utils/unoptional.py
+++ backend/src/utils/unoptional.py
@@ -1,14 +1,16 @@
 from typing import Type, TypeVar
 
+from fastapi import HTTPException
+
 T = TypeVar("T")
 
 
-def unoptional(elem: T | None, name_elem: str | None = None, to_raise: Type[Exception] = ValueError) -> T:
+def unoptional(elem: T | None, exception_message: str | None = None, to_raise: str = "ValueError") -> T:
     """Take an element which can be optional, and raise an error if it is None
 
     Args:
         elem (T | None): The checked element
-        name_elem (str | None, optional): The name of the element - For the raise message. Defaults to None.
+        exception_message (str | None, optional): The name of the element - For the raise message. Defaults to None.
 
     Raises:
         ValueError: If the element is None
@@ -17,6 +19,10 @@ def unoptional(elem: T | None, name_elem: str | None = None, to_raise: Type[Exce
         elem: the element without None Type
     """
     if elem is None:
-        raise ValueError(f"l'élément {name_elem} vaut None")
+        if to_raise:
+            if to_raise == "HttpException":
+                raise HTTPException(status_code=404, detail=exception_message)
+        
+        raise ValueError(exception_message)
     else:
         return elem
